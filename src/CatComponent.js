3e04ff33bdbae45c2e8dd6825c07a1e2ada93b0f
komald4 <komald.4@gmail.com>
2020-04-19 14:29:45 -0400

    Done.

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import CatComponent from './CatComponent.js'
+import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
 
-export default class CatComponent extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="bar" id="cat">
-        <img src="/cat.gif" />
+      <div className="App">
+        <CatComponent />
+        <GraceHopperQuoteComponent />
+        <MouseComponent />
       </div>
     );
   }
 }
+
+export default App;
